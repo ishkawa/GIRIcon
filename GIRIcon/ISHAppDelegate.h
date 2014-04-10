48f2e0a57959870304e88414e729ceebc2b5b8fe
Yosuke Ishikawa <y@ishkawa.org>
2014-04-10 22:20:11 +0900

    Initial Commit

---
+++ GIRIcon/ISHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISHAppDelegate.h
+//  GIRIcon
+//
+//  Created by Yosuke Ishikawa on 4/10/14.
+//  Copyright (c) 2014 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GIRIcon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GIRIcon
+//
+//  Created by Yosuke Ishikawa on 4/10/14.
+//  Copyright (c) 2014 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ISHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ISHAppDelegate class]));
+    }
+}
